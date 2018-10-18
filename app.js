71ff5015f48f412a132000d4d36f8e493fd1c87a
jekrb <jake@burden.blog>
2018-10-18 10:10:24 -0400

    check alarm after time UI updates

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ enableAlarmSetting(ui, state)
 function onTick (state) {
   const time = state
   return function () {
-    alarm.checkTime()
     const pm = time.hours >= 12
     let hours = pm ? time.hours - 12 : time.hours
     if (hours === 0) hours = 12
@@ -25,5 +24,6 @@ function onTick (state) {
     const seconds = time.seconds < 10 ? '0' + String(time.seconds) : time.seconds
     ui.clock.textContent = `${hours} : ${minutes} : ${seconds}`
     ui.am_pm.textContent = `${pm ? 'pm' : 'am'}`
+    alarm.checkTime()
   }
 }
